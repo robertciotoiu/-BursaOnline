48ffb3315f6a88a8c968c09d0626ca28fd32e45f
Robert Ciotoiu <robert.ciotoiu@gmail.com>
2019-11-30 19:06:47 +0200

    Add the project

---
+++ src/Buyer.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+import javax.jms.TopicPublisher;
+import javax.jms.TopicSession;
+import javax.naming.InitialContext;
+
+public class Buyer {
+	
+}
